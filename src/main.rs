f6db37fcca8830ab1a9efea0d11af41bc60d1a65
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-08-05 15:23:27 -0700

    setup logging and pass numbread and compression to introns

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ mod functions;
 mod io;
 mod types;
 mod utils;
+use std::time::SystemTime;
+
 use anyhow::Result;
 use bedrs::{GenomicInterval, GenomicIntervalSet};
 use clap::Parser;
@@ -14,16 +16,35 @@ pub type GivSet = GenomicIntervalSet<usize>;
 pub type NameMap = HashMap<Vec<u8>, usize>;
 pub type IdMap = HashMap<usize, Vec<u8>>;
 
+fn setup_logger() -> Result<()> {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "[{} {}] {}",
+                humantime::format_rfc3339_seconds(SystemTime::now()),
+                record.level(),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Debug)
+        .chain(std::io::stderr())
+        .apply()?;
+    Ok(())
+}
+
 fn main() -> Result<()> {
     let cli = Cli::parse();
+    setup_logger()?;
     match cli.command {
         Command::Introns {
             gtf,
             fasta,
             output,
             extension,
+            num_threads,
+            compression_level,
         } => {
-            functions::run_introns(&gtf, &fasta, output, extension)?;
+            functions::run_introns(&gtf, &fasta, output, extension, num_threads, compression_level)?;
         }
     }
     Ok(())
